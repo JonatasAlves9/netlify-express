fdfcb10b78780d46680af84ea605b9d22892cd2b
JonatasAlves9 <alvesjonatas99@gmail.com>
2020-12-10 12:58:37 -0300

    rota nova

--- express/server.js
+++ express/server.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 'use strict';
 const express = require('express');
 const path = require('path');
@@ -5,12 +6,31 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+
+const api = axios.create({
+  baseURL: 'https://multtvadmin.multtv.tv.br/OB/user/endpoint?wsdl',
+});
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+router.post('/test', async (request, response) => {
+  console.log("Teste")
+  const xml = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"   xmlns:bees="http://www.beesmart.tv/">    <soapenv:Header/>  <httpProtocol>      <customHeaders>        <add name="Access-Control-Allow-Origin" value="*" />      </customHeaders>    </httpProtocol>    <soapenv:Body>    <bees:getFilteredProfilesV4>       <profileFilter >      <regionUid >BALNEARIO_CAMBORIU</regionUid >    </profileFilter >    </bees:getFilteredProfilesV4> </soapenv:Body ></soapenv:Envelope >`;
+
+  try {
+    const { data } = await api.post('', xml);
+    console.log(data)
+    return response.json(data);
+  } catch (error) {
+    console.log(error)
+  }
+}); 
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
